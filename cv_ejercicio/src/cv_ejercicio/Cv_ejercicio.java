8d47d0f5a157d8370cf23f7e5251dff2de6f69b1
Usuario <Usuario@Usuario-PC>
2022-06-14 18:05:05 -0500

    rama Desarrollador1 con mensaje "version 1.2"

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -18,6 +18,7 @@ public class Cv_ejercicio {
         // TODO code application logic here
         System.out.println("version 1");
         System.out.println("version 1.1");
+        System.out.println("version 1.2");
     }
     
 }
